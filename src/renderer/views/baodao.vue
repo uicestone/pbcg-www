f7b1df1b3a210c1ec887921f09b5a773bc381162
Uice Lu <uicestone@gmail.com>
2019-03-02 22:36:01 +0800

    党员报到

--- src/renderer/views/baodao.vue
+++ src/renderer/views/baodao.vue
@@ -48,21 +48,22 @@ export default {
     <body>
     <div class="main page">
       <div class="header">
-        <a class="fl back" @click="goBack"><i class="fa fa-angle-left"></i> 党员报到</a>
+        <a class="fl back" @click="goBack"><i class="fa fa-angle-left"></i> 返回</a>
+        <h1>党员报到</h1>
       </div>
       <form @submit="submit">
       <div class="formDiv">
         <div class="inputDiv">
-          <span class="fl">姓名*</span>
+          <span class="fl">姓名</span>
           <input type="text" v-model="profile.name" placeholder="您的真实姓名" required />
         </div>
         <div class="inputDiv">
-          <span class="fl">手机*</span>
+          <span class="fl">手机</span>
           <input type="text" v-model="profile.mobile" placeholder="手机号码" required />
         </div>
         <div class="inputDiv">
-          <span class="fl">身份证</span>
-          <input type="text" v-model="profile.idCard" placeholder="用以核实您的身份" />
+          <span class="fl">单位</span>
+          <input type="text" v-model="profile.unit" placeholder="您所在的工作单位"/>
         </div>
         <div class="inputDiv">
           <span class="fl">性别</span>
@@ -70,24 +71,6 @@ export default {
             <span class="sex" @click="profile.sex='男'" :class="{check:profile.sex=='男'}">男</span>
             <span class="sex" @click="profile.sex='女'" :class="{check:profile.sex=='女'}">女</span>
           </div>
-        </div>
-        <div class="inputDiv">
-          <span class="fl">单位</span>
-          <input type="text" v-model="profile.unit" placeholder="您所在的工作单位"/>
-        </div>
-        <div class="inputDiv">
-          <span class="fl">所在党组织</span>
-          <input type="text" v-model="profile.organization" placeholder="您所在的党组织" class="zuzhi"/>
-          <!-- <i class="fa fa-angle-right fr"></i> -->
-        </div>
-        <div class="inputDiv">
-          <span class="fl">居住地</span>
-          <input type="text" v-model="profile.residence" placeholder="您所居住的地方"/>
-        </div>
-        <div class="inputDiv">
-          <span class="fl">特长</span>
-          <input type="text" v-model="profile.speciality" placeholder="您所擅长的工作、领域" class="techang"/>
-          <!-- <i class="fa fa-angle-right fr"></i> -->
         </div>
       </div>
       <div class="btnDiv">
